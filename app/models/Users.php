2ea8a4d803f5cd40b341d3896bc83f7599ca015b
prasertsak <prasertsak@prasertsak-PC>
2014-05-11 16:18:53 +0700

    add models

---
+++ app/models/Users.php
@@ -0,0 +1,12 @@
+<?php
+
+class Users extends \Phalcon\Mvc\Model
+{
+	public $id; 
+	public $name;
+	public $email;
+// 	public function getItems(){
+// 		//$iterms=array("name 1","name 2","name 3");		
+// 	}
+// 	//return $items;
+}
